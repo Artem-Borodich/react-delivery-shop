2ce543e283001a84433361733986088cf04d7f7f
Artem-Borodich <Nometa847@gmail.com>
2024-11-18 10:32:21 +0300

    io

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -2,11 +2,11 @@ const bcrypt = require('bcryptjs');
 
 const hashPassword = (password) => {
   const salt = bcrypt.genSaltSync();
-  return bcrypt.hashSync(password, salt);
+  return bcrypt.hashSync(password, salt); // хэшируем пароль
 };
 
 const comparePassword = (raw, hash) => {
-  return bcrypt.compareSync(raw, hash);
+  return bcrypt.compareSync(raw, hash); // проверяем совпадает ли хэш с изначальным
 };
 
 module.exports = {
